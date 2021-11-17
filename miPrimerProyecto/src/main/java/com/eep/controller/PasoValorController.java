c8a11ba8da0256c2400bdc877521a4a3a0a072df
Diego <dlloret@eepmad.com>
2021-11-17 19:32:28 +0100

    ejercicio1 terminado

--- miPrimerProyecto/src/main/java/com/eep/controller/PasoValorController.java
+++ miPrimerProyecto/src/main/java/com/eep/controller/PasoValorController.java
@@ -1,4 +1,4 @@
-package com.eep.controller;
+  package com.eep.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,19 +8,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/ejemplos")
-public class EjemploPasoValorController {
-	final String plantillaPasoValor="ejemplopasovalor";
+@RequestMapping("/ejercicio1")
+public class PasoValorController {
+	final String plantillaPasoValor="actividad2";
 	@GetMapping("/ejemplo1")
 	public String helloWorld(Model model) {
-		model.addAttribute("persona","juan");
+		model.addAttribute("escuela","Eep iGroup");
 		return plantillaPasoValor;
 	}
 	@RequestMapping(value="/ejemplo1mav", method=RequestMethod.GET)
 	public ModelAndView helloworldMAV() {
 		
 	ModelAndView mav= new ModelAndView(plantillaPasoValor);
-	mav.addObject("persona","pepe");
+	mav.addObject("escuela","Eep iGroup");
 	return mav;
 	}
 }
